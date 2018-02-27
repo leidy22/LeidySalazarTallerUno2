f0d902e0c85d673b7961fc46b4c2c21e2f2edeb6
leidy salazar <leidyjohanasv22@gmail.com>
2018-02-27 13:51:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aula7/leidysalazartalleruno/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aula7.leidysalazartalleruno;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
